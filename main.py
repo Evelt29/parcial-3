eb017479e192e012a34f58f4c3d9a92dc125cd97
Evelt29 <tovarevelyn2911@gmail.com>
2024-04-15 00:46:20 -0600

    cambios

---
+++ main.py
@@ -0,0 +1,8 @@
+from lib import *
+import pandas as pd
+
+nombre_archivo = input('Nombre del achivo:')
+df = pd.read_excel(nombre_archivo,index_col=0)
+print('\033[35m Matriz de adyacencia\033[0m')
+print(df)
+
